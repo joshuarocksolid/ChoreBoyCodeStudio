8a217117a510ef6cc7bf030a28ff23cdac552e8a
Joshua Aguilar <joshua@rocksoliddata.solutions>
2026-03-06 16:44:51 -0600

    Removed a bunch of large files

--- jasper_probe/tools/HelloJava.java
+++
@@ -1,9 +0,0 @@
-public class HelloJava {
-    public static void main(String[] args) {
-        System.out.println("HELLO_JAVA_OK|" +
-            System.getProperty("java.version") + "|" +
-            System.getProperty("java.vendor") + "|" +
-            System.getProperty("os.name") + "|" +
-            System.getProperty("os.arch"));
-    }
-}
